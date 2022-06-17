017a87420164f9a498b25251ab44befff62a5d99
Zacharier <42321025+Zacharier@users.noreply.github.com>
2022-06-17 14:57:37 +0800

    set version info

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from src.bukit import __version__
 
 setup(
     name="bukit",
     author="Zacharier",
+    version=__version__,
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={"console_scripts": ["bukit = bukit:main"]},
